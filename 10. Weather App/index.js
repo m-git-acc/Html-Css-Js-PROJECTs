7113de4bc9d6f1f5044e96ef770a69e951f1ad81
m-git-acc <mohitcodedeveloper@gmail.com>
2023-10-22 12:26:01 +0530

    added weather app

--- 10. Weather App/index.js
+++ 10. Weather App/index.js
@@ -2,7 +2,7 @@
 // const API_KEY = `3265874a2c77ae4a04bb96236a642d2f`
 // const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
 
-const API_KEY = '3265874a2c77ae4a04bb96236a642d2f';
+const API_KEY = '--';//input your api key on OPENWEATHER website.
 
 const form = document.querySelector("form");
 const search = document.querySelector("#search");
